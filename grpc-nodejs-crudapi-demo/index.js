1a828472e28400ed76503a760b55780188d8eb0c
linnefromice <linnefromice@gmail.com>
2020-04-12 12:34:23 +0900

    add datas & add client function

--- grpc-nodejs-crudapi-demo/index.js
+++ grpc-nodejs-crudapi-demo/index.js
@@ -1,7 +1,16 @@
 const grpc = require('grpc');
 const notes_proto = grpc.load('notes.proto');
+const notes = [
+  {id: "1", title: "Note 1", content: "Content 1"},
+  {id: "2", title: "Note 2", content: "Content 2"}
+];
 
 const server = new grpc.Server();
+server.addService(notes_proto.NoteService.service, {
+  list: (_, callback) => {
+    callback(null, notes)
+  },
+});
 
 server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
 console.log('Server running at http://127.0.0.1:50051');
