4713ae8db7b04179d1f98770f88c6f9bff6f239b
linnefromice <linnefromice@gmail.com>
2020-04-12 12:43:37 +0900

    add insert function - modify index.js (register function)

--- grpc-nodejs-crudapi-demo/index.js
+++ grpc-nodejs-crudapi-demo/index.js
@@ -1,5 +1,7 @@
 const grpc = require('grpc');
 const notes_proto = grpc.load('notes.proto');
+const uuidv1 = require('uuid/v1');
+
 const notes = [
   {id: "1", title: "Note 1", content: "Content 1"},
   {id: "2", title: "Note 2", content: "Content 2"}
@@ -10,6 +12,12 @@ server.addService(notes_proto.NoteService.service, {
   list: (_, callback) => {
     callback(null, notes)
   },
+  insert: (call, callback) => {
+    let note = call.request;
+    note.id = uuidv1();
+    notes.push(note);
+    callback(null, note);
+  }
 });
 
 server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
